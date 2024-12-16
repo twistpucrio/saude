4bce202fa6fc870e8ffdcf723827afb3df15bddf
Luisa Silveira <luisasilveira040@gmail.com>
2024-12-15 23:10:29 -0300

    tirando descrição hospital

--- JS/view/HospitalDetailsView.js
+++ JS/view/HospitalDetailsView.js
@@ -6,7 +6,6 @@ const HospitalDetailsView = (() => {
             <p><strong>Endereço:</strong> ${hospital.endereco}</p><br>
             <p><strong>Bairro:</strong> ${hospital.bairro.join(", ")}</p><br>
             <p><strong>Procedimentos:</strong> ${hospital.procedimentos.join(", ")}</p><br>
-            <p><strong>Descrição:</strong> ${hospital.descricao}</p>
             </div>
             <img src="${hospital.imagem}" alt="">
         `;
